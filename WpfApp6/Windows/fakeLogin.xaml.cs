62bfbc5ceaedc28fdb9a9753f83ed8f4e6c660fa
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-05-08 09:50:55 +0200

    :8ball:

---
+++ WpfApp6/Windows/fakeLogin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Launcher.Windows
+{
+    /// <summary>
+    /// Interaction logic for fakeLogin.xaml
+    /// </summary>
+    public partial class fakeLogin : Window
+    {
+        public fakeLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //Regex r = new Regex(@"^[a-zA-Z@]+$");
+            //if (!r.IsMatch(e.Text))
+            //e.Handled = true;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
